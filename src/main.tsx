351edc42176c3d38556f0fd11a5666963b14099d
Revathi <revathi9605@gmail.com>
2026-01-06 23:11:17 -0500

    Fix GitHub Pages blank screen1

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-   <StrictMode>
-      <BrowserRouter basename="/-Project_React_RestCountriesAPI">
-         <ThemeProvider>
-            <App />
-         </ThemeProvider>
-      </BrowserRouter>
-   </StrictMode>
-);
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//    <React.StrictMode>
-//       <ThemeProvider>
-//          <HashRouter basename="/-Project_React_RestCountriesAPI">
+// createRoot(document.getElementById("root")!).render(
+//    <StrictMode>
+//       <BrowserRouter basename="/-Project_React_RestCountriesAPI">
+//          <ThemeProvider>
 //             <App />
-//          </HashRouter>
-//       </ThemeProvider>
-//    </React.StrictMode>
+//          </ThemeProvider>
+//       </BrowserRouter>
+//    </StrictMode>
 // );
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+   <React.StrictMode>
+      <ThemeProvider>
+         <HashRouter basename="/-Project_React_RestCountriesAPI">
+            <App />
+         </HashRouter>
+      </ThemeProvider>
+   </React.StrictMode>
+);
