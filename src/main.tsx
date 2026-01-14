ec99692577099cdb7d97c35d1fcc44e9932894f7
Revathi <revathi9605@gmail.com>
2026-01-14 17:10:33 -0500

    Error fixed in main.rsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,16 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
 import App from "./App.tsx";
 
-// createRoot(document.getElementById("root")!).render(
-//    <StrictMode>
-//       <BrowserRouter basename="/-Project_React_RestCountriesAPI">
-//          <ThemeProvider>
-//             <App />
-//          </ThemeProvider>
-//       </BrowserRouter>
-//    </StrictMode>
-// );
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-   <React.StrictMode>
-      <ThemeProvider>
-         <HashRouter basename="/-Project_React_RestCountriesAPI">
+createRoot(document.getElementById("root")!).render(
+   <StrictMode>
+      <BrowserRouter>
+         <ThemeProvider>
             <App />
-         </HashRouter>
-      </ThemeProvider>
-   </React.StrictMode>
+         </ThemeProvider>
+      </BrowserRouter>
+   </StrictMode>
 );
